ea9135a08cf478c338f9d266e46848de5334e443
leeyongmin <trex0723@naver.com>
2025-03-10 16:01:08 +0900

    KNeihborsClassfiler를 사용한 붓꽃분류

--- irisModelBase.py
+++ irisModelBase.py
@@ -1,11 +1,10 @@
-# 2025.3.10.
+# 2025.3.10
 # 프로젝트2 붓꽃분류기 만들기
-# 이용희 교수님과 열심히 만들어보자
-import numpy as np
+import joblib
 import pandas as pd
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-
-
+from sklearn.ensemble import RandomForestClassifier
 
 iris_df = pd.read_csv('iris.csv')
 
@@ -14,15 +13,37 @@ X = iris_df.drop('species', axis=1)
 
 kn = KNeighborsClassifier()
 model_kn = kn.fit(X, y)
+rfc = RandomForestClassifier()
+model_rfc = rfc.fit(X, y)
+
+joblib.dump(model_rfc, 'model_rfc.pkl')
+joblib.dump(model_kn, 'model_kn.pkl')
 
 # X_new = np.array([[3,3,3,3]])
 # kn ['versicolor'] [[0.  0.8 0.2]]
-# X_new = np.array([[5.0, 3.4, 1.4, 0.2]])
-# kn ['versicolor'] [[1. 0. 0.]]
-X_new = np.array([[1, 4.2, 1.4, 7]])
-# kn ['versicolor'] [[0.2 0.6 0.2]]
-prediction = model_kn.predict(X_new)
-print(prediction)
-probability = model_kn.predict_proba(X_new)
-print(probability)
 
+# X_new = np.array([[1, 4.2, 1.4, 7]])
+# kn ['versicolor'] [[0.2 0.6 0.2]]
+
+# X_new = np.array([[1, 4.2, 1.4, 7]])
+# kn ['versicolor'] [[0.2 0.6 0.2]]
+# rfc ['setosa'] [[0.5  0.22 0.28]]
+
+X_new = np.array([[5.0, 3.4, 1.4, 0.2]])
+# kn ['setosa'][[1. 0. 0.]]
+# rfc ['setosa'][[1. 0. 0.]]
+
+model_kn = joblib.load('model_kn.pkl')
+model_rfc = joblib.load('model_rfc.pkl')
+
+kn_predict = model_kn.predict(X_new)
+rfc_predict = model_rfc.predict(X_new)
+
+kn_probability = model_kn.predict_proba(X_new)
+rfc_probability = model_rfc.predict_proba(X_new)
+
+print(kn_predict)
+print(kn_probability)
+
+print(rfc_predict)
+print(rfc_probability)
